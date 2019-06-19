f731e12d86a7c3cf0178be88729f55ed04a8e876
Crissavino <savinocristian89@gmail.com>
2019-06-19 10:29:44 -0300

    Subo para mostrar

--- routes/web.php
+++ routes/web.php
@@ -11,18 +11,18 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
 
 // Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
+// Route::get('/home', 'HomeController@index')->name('home');
 
-Route::get('/admin', 'Admin\AdminController@show');
+Route::get('#/admin', 'Admin\AdminController@show');
 
-Route::get('/admin/producto/nuevo', 'Admin\AdminController@createProducto');
-Route::post('/admin/producto/nuevo', 'Admin\AdminController@insertProducto');
-Route::get('/admin/producto/{id?}', 'Admin\AdminController@editProducto');
-Route::put('/admin/producto/{id}', 'Admin\AdminController@updateProducto');
-Route::delete('/admin/producto/borrar/{id}', 'Admin\AdminController@destroyProducto');
+Route::get('#/admin/producto/nuevo', 'Admin\AdminController@createProducto');
+Route::post('#/admin/producto/nuevo', 'Admin\AdminController@insertProducto');
+Route::get('#/admin/producto/{id?}', 'Admin\AdminController@editProducto');
+Route::put('#/admin/producto/{id}', 'Admin\AdminController@updateProducto');
+Route::delete('#/admin/producto/borrar/{id}', 'Admin\AdminController@destroyProducto');

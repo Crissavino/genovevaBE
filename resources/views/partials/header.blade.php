cc0e89542a8ed14590f2881443993a3453bc7a8c
Crissavino <savinocristian89@gmail.com>
2019-06-19 00:26:01 -0300

    Subo para mostrar

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,3 +1,4 @@
 <header class="">
-    <a class="btn btn-outline-danger m-4" href="http://localhost:4200/">Volver al shop</a>
+    <a class="btn btn-outline-danger m-4" href="http://genovevaok.com">Volver al shop</a>
+    {{-- <a class="btn btn-outline-danger m-4" href="http://localhost:4200/">Volver al shop</a> --}}
 </header>
